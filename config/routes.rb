337d7e6b762449bc666315173a7bd1563ab5e250
bernardoseven <bernardoseven@gmail.com>
2016-01-06 03:26:41 +0000

    added destroy route to sessions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ get 'signup'  => 'users#new'
 resources :users
 get '/login' => 'sessions#new'
 post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
